7fb284b55939a278374a9afbdedd273b07067d09
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-01-30 18:10:24 -0600

    Update 2 Baja California.py

--- pages/2 Baja California.py
+++ pages/2 Baja California.py
@@ -420,7 +420,7 @@ left_column.plotly_chart(fig11, use_container_width=True)
 right_column.plotly_chart(fig12, use_container_width=True)
 ############################################################################################
 
-st.link_button("Baja California-mapa", "https://mapa-2020-censo-inegi-1.000webhostapp.com/")
+st.link_button("Baja California-mapa", "https://2z6f2lrzc5fegrmyunpt2q.on.drv.tw/html/mapa1.html")
 
 
 
